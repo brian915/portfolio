0adc3da1a89569c2d5b4e7cd85ac47072b318b9d
brian915 <brian@brianrunk.net>
2024-12-10 20:33:53 -0500

    updated create blog post script

---
+++ _posts/2024-12-09-why-I-created-something-that-already-exists.md
@@ -0,0 +1,18 @@
+---
+layout: post
+title: Why I created something that already exists
+---
+
+#Why I created something that already exists
+
+
+- Copy notes from morning writing session
+
+
+
+
+
+- Generate Slug for Website and Linked In
+- Posts Section on Website
+
+
--- create_blog_post.rb
+++ create_blog_post.rb
@@ -2,6 +2,7 @@
 require 'date'
 require 'yaml'
 require 'optparse'
+require 'titlecaser'
 
 options = {}
 parse_options = OptionParser.new do |parser|
@@ -10,10 +11,8 @@ Usage: create_blog_post.rb [options] title.
 
 Generates a new blog file in the Posts directory a local Jekyll install in the format of #YEAR-MONTH-DAY-title.md
 "
-
    parser.on("-v", "--[no-]verbose", "Run verbosely") do |v|
      options[:verbose] = v
-    
      print parser.banner
      print "\n"
    end
@@ -22,11 +21,9 @@ Generates a new blog file in the Posts directory a local Jekyll install in the f
      print "no 'blogtitle' option specified as a parameter\n" + parser.banner + "\n"
      exit
    end
-  
 end.parse!
 
-
-
+@verbose = options[:verbose]
 
 @blog_title = parse_options.join("-")
 
@@ -34,7 +31,6 @@ end.parse!
 
 @blog_post_vars = YAML.load(File.read(@config_file))
 @file_location = @blog_post_vars['JEKYLL_POSTS_FOLDER']         
-
 
 class BlogPost
   attr_accessor :file_location, :file_name, :blog_title
@@ -53,49 +49,43 @@ class BlogPost
   def create_blog_post
     @blog_post_file_name = generate_file_name(blog_title)
     @full_path_file_name = @file_location + @blog_post_file_name
-    
+    @formatted_blog_title =  format_blog_title(blog_title)
     unless File.exist?(@full_path_file_name)
-
-      msg =  "blog_post_file_name " + @blog_post_file_name + "\n"
-      msg += "file_path " + @file_location + "\n"
-      msg += "full_path_file_name " + @full_path_file_name + "\n"
-      print_msg(msg)
-      
       new_file = File.new(@full_path_file_name,"w")
       new_file.puts "---"
       new_file.puts "layout: post"
-      new_file.puts "title:  " + blog_title.remove_dashes_replace_with_spaces_regex
+      new_file.puts "title:  " + @formatted_blog_title
       new_file.puts "---"
       new_file.close
-
-      #File.open(@full_path_file__name) do |f|
-       # f.each do |line|
-       #  puts line
-       #end
-      #end
-
-    #if success print file contents with message
     else
       msg = "file already exists for " + @blog_post_file_name
       print_msg(msg)
       exit
     end
+    if File.exist?(@full_path_file_name)
+      msg = "Success! Post created."  + "\n"
+      msg += read_blog_post(@full_path_file_name)
+      print_msg(msg)
+    else
+      msg = "Blog post" + @formatted_blog_title + " not created"
+      print_msg(msg)
+      exit
+    end
+  end
+
+  def read_blog_post(blog_file)
+    contents = File.read(blog_file)
+  end
+ 
+  def format_blog_title(blog_title)
+   blog_title = blog_title.gsub("-"," ")
+   Titlecaser::TitleCase.new(blog_title).convert
   end
 
   def generate_file_name(blog_title)
-    #YEAR-MONTH-DAY-title.MARKUP
     @blog_title = blog_title
     @file_name = Date.today.strftime("%Y-%m-%d")
     @file_name += "-" + @blog_title + ".md"
-  end
-
-  def print_file_name(file_name)
-    print "\n"
-  end
-
-  def print_file_location
-    print "file_location: " + @file_location
-    print "\n"
   end
 
   private
@@ -113,20 +103,11 @@ end
 blog_post = BlogPost.new(@file_location, @blog_title)
 blog_post.create_blog_post
 
-if options[:verbose] == true
-   print "\noptions[:verbose] = " +  options[:verbose].to_s
-   print "\n@blog_title = "
-   print @blog_title
-   print "\n"
-end
+#if @verbose == true
+#   print "\noptions[:verbose] = " +  options[:verbose].to_s
+#   print "\n"
+#end
 
-
-### next steps:
-### - pass title into Blog Create
-### - generate title
-### - create a new file in the posts directory
-### - add notes to README
-### - add blog post "why I built this?"
 
 
 
